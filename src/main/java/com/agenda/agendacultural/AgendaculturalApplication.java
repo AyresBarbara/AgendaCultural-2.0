3082ca3c8899f7c6da1ab0606f1b4beb949fd4a9
aluno <aluno@embarquedigital.portodigital.local>
2026-03-11 16:33:31 -0300

    Configuração do pom

---
+++ src/main/java/com/agenda/agendacultural/AgendaculturalApplication.java
@@ -0,0 +1,16 @@
+package com.agenda.agendacultural;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Agenda Cultural API", version="1", description="API para gerenciamento de eventos culturais, categorias, comentários e favoritos."))
+public class AgendaculturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaculturalApplication.class, args);
+	}
+
+}
